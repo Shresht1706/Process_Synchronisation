a81b872033f32d815b073d26548f5a8a4acbc6be
Shresht1706 <156106644+Shresht1706@users.noreply.github.com>
2024-11-14 21:42:46 +0100

    v3 w proper execute jobs

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h> // For sleep function
+#include <unistd.h>  // For sleep function
 #include <pthread.h> // For threading
 
-#define Num_Users 5 // Number of users (threads)
-#define Num_Jobs_Per_User 10
+#define Num_Users 5           // Number of users (threads)
+#define Num_Jobs_Per_User 10  // Number of jobs per user
 #define Job_Length_Types 3
-#define Num_Shared_Var 2
 
 typedef enum { PRINT, SCAN } JobType;
 
@@ -21,50 +20,70 @@ typedef struct {
 int printer = 0;
 int scanner = 0;
 
+// Function to generate a single job for a user
+Job generate_job(char user, int job_index) {
+    Job job;
+    job.user = user;
+    job.job_type = (rand() % 2 == 0) ? PRINT : SCAN;
 
-// Function to generate and print 10 jobs for a user
-void *generate_job(void *arg) {
+    // Determine job length
+    int length_type = rand() % Job_Length_Types;
+    int min_pages, max_pages;
+    switch (length_type) {
+        case 0:  // SHORT
+            min_pages = 1;
+            max_pages = 5;
+            break;
+        case 1:  // MEDIUM
+            min_pages = 6;
+            max_pages = 15;
+            break;
+        case 2:  // LARGE
+            min_pages = 16;
+            max_pages = 50;
+            break;
+    }
+    job.length = min_pages + (rand() % (max_pages - min_pages + 1));
+
+    // Calculate arrival time as a random number between 1 and 5 + (5 * job_index)
+    job.arrival_time = (rand() % 5 + 1) + (5 * job_index);
+
+    return job;
+}
+
+// Function to execute 10 jobs for each user without synchronization
+void *execute_jobs(void *arg) {
     char user = *(char *)arg;
 
     for (int i = 0; i < Num_Jobs_Per_User; i++) {
-        Job job;
-        job.user = user;
-        job.job_type = (rand() % 2 == 0) ? PRINT : SCAN;
+        // Generate a job
+        Job job = generate_job(user, i);
 
-        // Determine job length
-        int length_type = rand() % Job_Length_Types;
-        int min_pages, max_pages;
-        switch (length_type) {
-            case 0:  // SHORT
-                min_pages = 1;
-            max_pages = 5;
-            break;
-            case 1:  // MEDIUM
-                min_pages = 6;
-            max_pages = 15;
-            break;
-            case 2:  // LARGE
-                min_pages = 16;
-            max_pages = 50;
-            break;
+        // Wait for the job's arrival time to simulate delay
+        sleep(job.arrival_time);
+
+        // Announce the start of the job
+        printf("User %c is starting a %s job. Length: %d pages, Arrival Time: %d\n",
+               job.user, (job.job_type == PRINT ? "PRINT" : "SCAN"), job.length, job.arrival_time);
+
+        // Check job type and process each page with a delay
+        if (job.job_type == PRINT) {
+            for (int page = 1; page <= job.length; page++) {
+                printf("User %c printing page %d of %d\n", job.user, page, job.length);
+                sleep(1);  // Simulate time taken to print each page
+            }
+            printf("User %c finished printing %d pages.\n", job.user, job.length);
+        } else {
+            for (int page = 1; page <= job.length; page++) {
+                printf("User %c scanning page %d of %d\n", job.user, page, job.length);
+                sleep(1);  // Simulate time taken to scan each page
+            }
+            printf("User %c finished scanning %d pages.\n", job.user, job.length);
         }
-        job.length = min_pages + (rand() % (max_pages - min_pages + 1));
-
-        // Calculate arrival time as a random number between 1 and 5 + (5 * i)
-        job.arrival_time = (rand() % 5 + 1) + (5 * i);
-
-        // Print job details
-        printf("User: %c, Job %d, Type: %s, Length: %d, Arrival Time: %d\n",
-               job.user, i + 1,
-               (job.job_type == PRINT ? "PRINT" : "SCAN"),
-               job.length, job.arrival_time);
     }
 
     return NULL;
 }
-
-// Function to simulate a job
-
 
 int main() {
     srand(time(NULL)); // Seed random number generator
@@ -76,7 +95,7 @@ int main() {
 
     // Create threads for each user
     for (int i = 0; i < Num_Users; i++) {
-        pthread_create(&threads[i], NULL, generate_job, (void *)&users[i]);
+        pthread_create(&threads[i], NULL, execute_jobs, (void *)&users[i]);
     }
 
     // Wait for all threads to finish
